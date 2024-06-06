e2e837f7c19c2a82cbf96091e84c71c8465cc096
NA-Black <ton09172010@gmail.com>
2024-06-06 15:48:11 +0800

    Created class about timesheet

---
+++ Ayrton_Bautista/tracker.py
@@ -0,0 +1,25 @@
+import time
+
+class time_sheet:
+    def __init__(self, name, total_balance):
+        self.name = name
+        self.time = [0] * 7 # initialize to be zero
+        self.total_balance = total_balance
+
+    
+    def summary_week(self):
+        time = 0
+        balance = 0
+        free_day = True
+
+        for i in self.time:
+            time = self.time[i]
+
+            if self.time[i] < 60*60:
+                if free_day == True:
+                    free_day = False
+                else:
+                    balance += 20
+
+        self.total_balance += balance
+
